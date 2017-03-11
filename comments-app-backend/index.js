1802c24d56943f50323e21b64832357c2daf9b83
yonatanayalon <yonatanayalon1@gmail.com>
2017-03-11 12:57:06 +0200

    Update index.js

--- comments-app-backend/index.js
+++ comments-app-backend/index.js
@@ -6,7 +6,7 @@ const database = require('./database')
 app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
+  res.header('Access-Control-Allow-Methods', 'GET, PUT, DELETE');
   next();
 });
 
